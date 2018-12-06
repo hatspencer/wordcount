8c8578b135177e9717f2dfbad8433edf6b5daa96
jakob oberhummer <office@chilimatic.com>
2018-12-06 16:20:39 +0100

    iteration 4 - session 5
     - adds getUniqueWords
     - changes System.out to utilize format
     - adds new - delimiter to words splitting

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,18 +1,31 @@
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class WordCount {
 
     public static void main (String[] args) throws IOException {
-        String pathToInputFile = args[0];
+        System.out.println("Please enter a text:");
+        final Scanner scanner = new Scanner(System.in);
+        final String words = scanner.nextLine();
 
         final String pathToStopWords = "/stopwords.txt";
-        final String[] wordSet = filterStopWords(
-                getWordsFromFile(pathToInputFile),
+        final String[] filteredWords = filterStopWords(
+                getWords(words),
                 getWordsFromFile(pathToStopWords)
         );
 
-        System.out.println("Number of Words:" + wordSet.length);
+        final Set<String> uniqueWords = getSetOfUniueWords(filteredWords);
+
+        System.out.println(
+                String.format(
+                        "Number of Words: %s. Number of unique words: %s",
+                        filteredWords.length,
+                        uniqueWords.size()
+                )
+        );
     }
 
     private static String[] getWordsFromFile(String pathToStopWords) throws IOException {
@@ -25,8 +38,18 @@ public class WordCount {
         );
     }
 
+    private static Set<String> getSetOfUniueWords(String[] words) {
+        Set<String> hashSet = new HashSet<>();
+
+        for (String word : words) {
+            hashSet.add(word);
+        }
+
+        return hashSet;
+    }
+
     public static String[] getWords(String words) {
-        return words.split("\\s");
+        return words.split("\\s|-");
     }
 
     public static String[] filterStopWords(String[] wordSet, String[] stopWords) {
