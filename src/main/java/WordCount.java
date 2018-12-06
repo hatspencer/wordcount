2f0dd465ae5bc491a20f9819c6448f84bef82b53
jakob oberhummer <office@chilimatic.com>
2018-12-06 10:53:48 +0100

    state Session-2 iteration 1
     - removes unnecessary class
     - moves matching to a static method within the main class
     - adds jdoc blocks

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,43 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCount {
 
+    /**
+     * @param args
+     */
     public static void main (String[] args){
         System.out.println("Please enter a text:");
         Scanner scanner = new Scanner(System.in);
 
         final String words = scanner.next();
+        final String[] wordSet = WordCount.extractWords(words);
 
-        final WordExtractor wordExtractor = new WordExtractor(words);
-
-        System.out.println("Number of Words:" + wordExtractor.getWords().length);
+        System.out.println("Number of Words:" + wordSet.length);
     }
 
-}
+    /**
+     * @param words
+     * @return
+     */
+    private static String[] extractWords(String words) {
+        /*
+         * taken from
+         * https://stackoverflow.com/questions/6020384/create-array-of-regex-matches
+         */
+        List<String> allMatches = new ArrayList<>();
+        Matcher m = Pattern.compile("(a-zA-Z)+")
+                .matcher(words);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
 
-class WordExtractor {
 
-    private String words;
-
-    private String[] wordSet;
-
-    public WordExtractor(String words) {
-        this.words = words;
-
-
+        return allMatches.toArray(new String[0]);
     }
-
-    private void extractWords() {
-        Pattern wordPattern = Pattern.compile("(a-zA-Z)+");
-        Matcher wordMatcher = wordPattern.matcher(this.words);
-
-        this.wordSet = wordPattern.split("\\s");
-    }
-
-    public String[] getWords() {
-        return this.wordSet;
-    }
-
 }
