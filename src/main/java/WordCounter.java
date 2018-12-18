f61580ab7d67ff8ad1bd6f91a66b12742f38a0ae
Mark Thomson <thomsonm@simplity.eu>
2018-12-18 13:44:40 +0100

    Refactoring and adding test cases

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -7,16 +7,24 @@ public class WordCounter {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter text: ");
-        System.out.println("Number of words: " + countWords(reader.nextLine()));
+        String input = reader.nextLine();
+        if (isValidInput(input)) {
+            System.out.println("Number of words: " + countWords(input));
+        } else {
+            System.out.println("Invalid input! Please only enter lower-case, upper-case or whitespace characters only");
+        }
     }
 
-    private static long countWords(String text) {
-        Pattern alphaOnlyPattern = Pattern.compile("\\b[A-Za-z]+\\b");
-        Matcher matcher = alphaOnlyPattern.matcher(text);
-        int count = 0;
-        while (matcher.find()) {
-            count++;
+    static boolean isValidInput(String input) {
+        Pattern alphaAndWhitespaceOnlyPattern = Pattern.compile("^[A-Za-z ]*$");
+        Matcher matcher = alphaAndWhitespaceOnlyPattern.matcher(input);
+        return matcher.find();
+    }
+
+    static long countWords(String text) {
+        if (text.isEmpty()) {
+            return 0;
         }
-        return count;
+        return text.split("\\s+").length;
     }
 }
--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,5 +1,73 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class WordCounterTest {
 
+    @Test
+    public void isValidInputShouldReturnFalseOnInvalidInput() {
+        String invalidInput = "foo bar1";
 
+        boolean result = WordCounter.isValidInput(invalidInput);
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValidInputShouldReturnTrueOnValidInput() {
+        String validInput = "foo bar";
+
+        boolean result = WordCounter.isValidInput(validInput);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void countWordsReturnsCorrectNumberOfWords() {
+        String twoWords = "foo bar";
+
+        long result = WordCounter.countWords(twoWords);
+
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void countWordsReturnsCorrectNumberOfWordsWithMultipleSpaces() {
+        String twoWords = "foo     bar";
+
+        long result = WordCounter.countWords(twoWords);
+
+        assertEquals(2, result);
+    }
+
+
+    @Test
+    public void isValidInputShouldReturnTrueOnNoInput() {
+        String noInput = "";
+
+        boolean result = WordCounter.isValidInput(noInput);
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void countWordsReturnsZeroOnNoInput() {
+        String noInput = "";
+
+        long result = WordCounter.countWords(noInput);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void countWordsReturnsZeroOnEmptyInput() {
+        String emptyInput = " ";
+
+        long result = WordCounter.countWords(emptyInput);
+
+        assertEquals(0, result);
+    }
 
 }
