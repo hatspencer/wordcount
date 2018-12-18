ed16994da8f40461fe9ed0178c9221bb6b3855bf
polacla1 <claus.polanka@s-itsolutions.at>
2018-12-18 15:52:12 +0100

    Change instance to static call

--- src/main/java/com/erstegroup/hiring/WordCountApp.java
+++ src/main/java/com/erstegroup/hiring/WordCountApp.java
@@ -19,7 +19,7 @@ public class WordCountApp {
         }
 
         if (InputValidator.validate(text)) {
-            List<String> stopWords = new StopWordsProvider().readStopWords();
+            List<String> stopWords = StopWordsProvider.readStopWords();
             System.out.println("Number of words: " + WordCount.wordCount(text, stopWords));
         } else {
             System.err.println("Error input");
