d9405dd73009434f3127aa2d1e067e858104bc3e
Daniel Hladik <daniel.hladik@isdd.sk>
2019-03-06 12:11:52 +0100

    Word Count 2 - test + refactor

--- src/test/java/FileReaderTest.java
+++ src/test/java/FileReaderTest.java
@@ -6,17 +6,17 @@ import org.junit.jupiter.api.Test;
 
 public class FileReaderTest {
 
-    private static final String GOOD_FILENAME = "filereadertest.txt";
-    private static final String BOGUS_FILENAME = "fwafwafwafwa.txt";
+    private static final String EXISTING_FILENAME = "filereadertest.txt";
+    private static final String NONEXISTING_FILENAME = "fwafwafwafwa.txt";
 
     @Test
     public void readFileNonExistingFileExpectEmptyList() {
-        List<String> lines = FileReader.readFileAsLines(BOGUS_FILENAME);
+        List<String> lines = FileReader.readFileAsLines(NONEXISTING_FILENAME);
         Assertions.assertTrue(lines.isEmpty());
     }
 
     @Test
     public void readFileExpectListWithFourItems() {
-        Assertions.assertEquals(4, FileReader.readFileAsLines(GOOD_FILENAME).size());
+        Assertions.assertEquals(4, FileReader.readFileAsLines(EXISTING_FILENAME).size());
     }
 }
