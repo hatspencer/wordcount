427873e8607451c2d04a4a52113f37f61a6b871a
Huba Roman <roman.huba@isdd.sk>
2019-03-15 11:11:29 +0100

    iteration 1 - added junit test cases

--- src/test/java/RegexCounterTest.java
+++ src/test/java/RegexCounterTest.java
@@ -1,20 +1,40 @@
-import api.WordCounter;
 import impl.RegexCounter;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class RegexCounterTest {
 
-	public static void main(String[] args) {
-		String[] inputs = new String[]{"", null, "   ", "one", "one two.word", "? word word.", "Mary had a little lamb", "word?one.two three"};
-		int[] expectedResults = new int[]{0, 0, 0, 1, 2, 2, 5, 4};
-
-		WordCounter counter = new RegexCounter();
-		for (int i = 0; i < inputs.length; i++) {
-			int count = counter.count(inputs[i]);
-			if (count != expectedResults[i]) {
-				System.err.println("Expected different word count! Expected " + expectedResults[i] + " and got " + count + " for input '" + inputs[i] + "'");
-			} else {
-				System.out.println("Correct!");
-			}
-		}
+	@ParameterizedTest
+	@ValueSource(strings = {"", "    "})
+	void testEmptyString(String empty) {
+		assertEquals(0, new RegexCounter().count(empty));
 	}
+
+	@Test
+	void testNull() {
+		assertEquals(0, new RegexCounter().count(null));
+	}
+
+	@ParameterizedTest
+	@MethodSource("wordCountAndInputProvider")
+	void testWordBoundary(int wordCount, String input) {
+		assertEquals(wordCount, new RegexCounter().count(input));
+	}
+
+	static Stream<Arguments> wordCountAndInputProvider() {
+		return Stream.of(
+				arguments(2, "one two"),
+				arguments(2, "one.two"),
+				arguments(2, "one?two")
+		);
+	}
+
 }
