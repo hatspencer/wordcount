dd2620a557505edabe990d05d54e58cfa6f07e01
miro.makys <miro.makys@gmail.com>
2019-07-02 14:45:36 +0200

    Extend functionality of WordCounter to support input of collections
    Add tests for WordCounter

--- src/main/java/wordcount/WordCounter.java
+++ src/main/java/wordcount/WordCounter.java
@@ -1,17 +1,26 @@
 package wordcount;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class WordCounter {
 
 	private static final String WHITE_SPACE_REGEX = "\\s+";
 	private static final String ONLY_LETTERS_REGEX = "[a-zA-Z]+";
-	
+
 	private final Predicate<String> wordsFilter;
 
 	public WordCounter(Predicate<String> wordsFilter) {
 		this.wordsFilter = wordsFilter;
+	}
+
+	public long countWordsOf(Collection<String> text) {
+		return text.stream()
+				.filter(Objects::nonNull)
+				.mapToLong(this::countWordsOf)
+				.sum();
 	}
 
 	public long countWordsOf(String text) {
--- src/test/java/wordcount/WordCountTest.java
+++ src/test/java/wordcount/WordCountTest.java
@@ -1,12 +1,18 @@
 package wordcount;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class WordCountTest {
 
+	private final StopWordsFilter stopWordsFilter = new StopWordsFilter(Arrays.asList("a", "the"));
+	
 	@Test
-	public void testWordCounter() {
+	public void testWordCounterSingleString() {
 		WordCounter wordCounter = new WordCounter(x -> true);
 		
 		Assert.assertEquals(1, wordCounter.countWordsOf("a"));
@@ -14,5 +20,39 @@ public class WordCountTest {
 		Assert.assertEquals(2, wordCounter.countWordsOf("a a"));
 		Assert.assertEquals(2, wordCounter.countWordsOf("abc	abc"));
 		Assert.assertEquals(1, wordCounter.countWordsOf(" abc	 a@bc"));
+	}
+	
+	@Test
+	public void testWordCounterSingleStringWithFilter() {
+		
+		WordCounter wordCounter = new WordCounter(stopWordsFilter);
+		
+		Assert.assertEquals(0, wordCounter.countWordsOf("a"));
+		Assert.assertEquals(1, wordCounter.countWordsOf("a the abc"));
+		Assert.assertEquals(2, wordCounter.countWordsOf("abc	abc"));
+	}
+	
+	@Test
+	public void testWordCounterStringEmptyCollection() {
+		WordCounter wordCounter = new WordCounter(x -> true);
+		
+		Assert.assertEquals(0, wordCounter.countWordsOf(Collections.emptyList()));
+		
+	}
+	
+	@Test
+	public void testWordCounterStringCollection() {
+		WordCounter wordCounter = new WordCounter(x -> true);
+		
+		List<String> text = Arrays.asList("", "a", "abc ab1c a@bc");
+		Assert.assertEquals(2, wordCounter.countWordsOf(text));
+	}
+	
+	@Test
+	public void testWordCounterStringCollectionWithFilter() {
+		WordCounter wordCounter = new WordCounter(stopWordsFilter);
+		
+		List<String> text = Arrays.asList("", "a", "abc ab1c a@bc");
+		Assert.assertEquals(1, wordCounter.countWordsOf(text));
 	}
 }
