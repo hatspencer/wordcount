6ce4f30325ececdf5ed1183bd115eb5603f98be4
miro.makys <miro.makys@gmail.com>
2019-07-02 16:42:15 +0200

    improve readability of StartTest

--- src/test/java/wordcount/StartTest.java
+++ src/test/java/wordcount/StartTest.java
@@ -1,6 +1,6 @@
 package wordcount;
 
-import java.util.Collections;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -8,7 +8,10 @@ import org.junit.Test;
 public class StartTest {
 	
 	@Test
-	public void countWordsReturns2() {
-		Assert.assertEquals(1, Start.countWords(Collections.singleton("word word2 a the")));
+	public void countWordsReturnsOnlyCountableWords() {
+		final String countableWord = "word"; // to be counted
+		final String notOnlyLettersWord = "word2"; // not to be counted
+		final String stopWord = "a"; // not to be counted
+		Assert.assertEquals(1, Start.countWords(Arrays.asList(countableWord, notOnlyLettersWord, stopWord)));
 	}
 }
