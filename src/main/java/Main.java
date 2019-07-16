04a25b5a7d9cb6c31c821a15b81efc0344b3995d
Barbraiiskii Nikolai <barbar@applifting.cz>
2019-07-16 10:47:11 +0200

    get stop words refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,20 +11,23 @@ public class Main {
         System.out.print("Enter text: ");
         String text = scanner.nextLine();
         scanner.close();
-        List<String> input =  new ArrayList<String>(Arrays.asList(text.trim().split("\\s+")));
+        List<String> input = new ArrayList<String>(Arrays.asList(text.trim().split("\\s+")));
         Iterator<String> interator = input.iterator();
-        while (interator.hasNext()){
-            if(!interator.next().matches("[a-zA-Z]+\\.?")){
+        while (interator.hasNext()) {
+            if (!interator.next().matches("[a-zA-Z]+\\.?")) {
                 interator.remove();
             }
         }
         return input;
     }
 
-    public static void main(String[] args) {
+    private static File getStopWordsFile() {
         ClassLoader classLoader = new Main().getClass().getClassLoader();
-        File excludedFile = new File(classLoader.getResource(EXCLUDED_FILE_NAME).getFile());
-        ExcludedWords excludedWords = new ExcludedWords(excludedFile);
+        return new File(classLoader.getResource(EXCLUDED_FILE_NAME).getFile());
+    }
+
+    public static void main(String[] args) {
+        ExcludedWords excludedWords = new ExcludedWords(getStopWordsFile());
         List<String> textList = readUsersInput();
         textList.removeAll(excludedWords.getExcludedWords());
         System.out.println("Number of words: " + textList.size());
