26cab401c983323ab3426dd0b2d71c7aee8660e2
Petr Hovorka <petr.hovorka@simplity.eu>
2019-07-25 14:22:23 +0200

    #1 First iteration - finished

--- src/test/java/eu/wordcount/InputLineWordReaderTest.java
+++ src/test/java/eu/wordcount/InputLineWordReaderTest.java
@@ -4,9 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class InputLineWordReaderTest {
 
@@ -15,15 +17,29 @@ public class InputLineWordReaderTest {
 
     @Before
     public void setUp() throws Exception {
-        inputStream = new ByteArrayInputStream("Mary had a little lamb\n".getBytes());
-        underTest = new InputLineWordReader(inputStream);
+
     }
 
     @Test
     public void whenInputStreamSuppliesTheSentence_thenItShouldBeReturnedByTheWordReader() {
-
+        inputStream = new ByteArrayInputStream("Mary had a little lamb\n".getBytes());
+        underTest = new InputLineWordReader(inputStream);
         final String actual = underTest.readWords();
         final String expected = "Mary had a little lamb";
         assertEquals("Sentence should be as expected", expected, actual);
+    }
+
+    @Test
+    public void whenInputStreamProducesIOException_thenShouldReturnNull() {
+        inputStream = new InputStream() {
+            @Override
+            public int read() throws IOException {
+                throw new IOException();
+            }
+        };
+        underTest = new InputLineWordReader(inputStream);
+
+        String actual = underTest.readWords();
+        assertNull("Reader should return null", actual);
     }
 }
