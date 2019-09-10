a733bbfe230258d0a69300a30511888eb01116d7
László Tamás Pintér <pintert@gain.uni-sopron.hu>
2019-09-10 10:13:36 +0200

    Added function to receive words contained in stopwords.txt. Changed language requirements to level 7 because of try-with-resources.

--- src/main/java/WordCountApplication.java
+++ src/main/java/WordCountApplication.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class WordCountApplication {
@@ -9,12 +15,28 @@ public class WordCountApplication {
         System.out.print("Number of words:" + numberOfWords);
 
     }
-    static int countWordsInText(String input){
+
+    static int countWordsInText(String input) {
         String[] wordsInInput = input.split(" ");
         int countOfWords = 0;
         for (String s : wordsInInput) {
             if (s.matches("[A-z]+")) countOfWords++;
         }
         return countOfWords;
+    }
+
+    static String[] getInvalidWords() {
+        List<String> invalidWords = new ArrayList<>();
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader("stopwords.txt"))) {
+            while ((line = reader.readLine()) != null) {
+                invalidWords.add(line);
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("stopwords.txt not found");
+        } catch (IOException ex) {
+            System.out.println("Cannot open file");
+        }
+        return invalidWords.toArray(new String[0]);
     }
 }
