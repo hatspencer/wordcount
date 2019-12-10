f55a115fe9fbdb3991b04ed24ec0b98e46f58ea4
Nándor Előd Fekete <nandor.fekete@gmail.com>
2019-12-10 15:05:11 +0100

    Added an initial implementation of StopWordFilter

--- src/main/java/wordcount/WordCounter.java
+++ src/main/java/wordcount/WordCounter.java
@@ -1,18 +1,34 @@
 package wordcount;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
- * Counts the words returned by the {@link Tokenizer}
+ * Counts the words returned by the {@link Tokenizer}.
  * 
  * @author Nándor Előd Fekete
  */
 public class WordCounter {
 
+    private final Predicate<String> filter;
+
+    public WordCounter() {
+        this(new StopWordsFilter(Collections.emptyList()));
+    }
+
+    public WordCounter(Predicate<String> filter) {
+        this.filter = filter;
+    }
+
     public long wordCount(final Tokenizer tokenizer) {
         long wordCount = 0;
-        while (tokenizer.nextToken().isPresent()) {
-            wordCount++;
+        Optional<String> currentToken;
+        while ((currentToken = tokenizer.nextToken()).isPresent()) {
+            if (currentToken.filter(filter).isPresent()) {
+                wordCount++;
+            }
         }
         return wordCount;
     }
