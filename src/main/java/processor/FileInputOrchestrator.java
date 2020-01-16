cfbb264a263fb11559ac79ed62ca9e24fe58b8b0
Jozef Morvay <jozef.morvay@ferratum.com>
2020-01-16 14:33:22 +0100

    Fix file input extension

--- src/main/java/processor/FileInputOrchestrator.java
+++ src/main/java/processor/FileInputOrchestrator.java
@@ -2,10 +2,8 @@ package processor;
 
 import wordcounter.SimpleWordCounter;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileInputOrchestrator extends WordCountOrchestrator{
