f085d97a8eac9da253a6f8975adcf57c248779df
jozefKruszynski <jozef.kruszynski+github@gmail.com>
2020-01-23 12:05:20 +0100

    Initial Scanner and System out message.

---
+++ src/main/java/WordCount.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+
+        String potentialWords = userInput.nextLine();
+
+        System.out.print("Enter text:");
+
+    }
+}
