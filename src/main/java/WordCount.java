9a79416f48e986cf9943e0103c60fc179575d985
jozefKruszynski <jozef.kruszynski+github@gmail.com>
2020-01-23 15:02:54 +0100

    Added test for empty string and for words number to be equal

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -7,18 +7,23 @@ public class WordCount {
     private static final String PROMPT = "Enter text: ";
     private static final String OUTPUT_PREFIX = "Number of words: ";
     private static int wordCount = 0;
+    private static String inputString;
 
     public static void main(String[] args) {
         System.out.print(PROMPT);
         Scanner userInput = new Scanner(System.in);
-        String inputString = userInput.nextLine();
-        String[] potentialWords = inputString.split("\\s");
+        inputString = userInput.nextLine();
+        countWordsInUserInput(inputString);
+        System.out.println(OUTPUT_PREFIX.concat(String.valueOf(wordCount)));
+    }
 
+    static int countWordsInUserInput(String userInput) {
+        String[] potentialWords = userInput.split("(\\s)");
         for (String word : potentialWords) {
             if (!word.isEmpty()) {
                 wordCount++;
             }
         }
-        System.out.println(OUTPUT_PREFIX.concat(String.valueOf(wordCount)));
+        return wordCount;
     }
 }
--- src/test/java/WordCountTest.java
+++ src/test/java/WordCountTest.java
@@ -1,7 +1,21 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 
 public class WordCountTest {
 
-    //@Test
-    //private void
+    @Test
+    public void expectEmptyStringNotToIncreaseWordCount() {
+
+        String emptyString = "";
+        Assertions.assertEquals(0,  WordCount.countWordsInUserInput(emptyString));
+    }
+
+    @Test
+    public void expectWordCountEqualToNumberOfWordsInInputString() {
+
+        String userInputString = "Foo Bar";
+        Assertions.assertEquals(2, WordCount.countWordsInUserInput(userInputString));
+    }
+
 }
