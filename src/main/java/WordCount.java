be4e4dea5b1f02767ab6967c15cda9802cd7757c
jozefKruszynski <jozef.kruszynski+github@gmail.com>
2020-01-23 12:48:31 +0100

    Iteration 1 complete.

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Pattern;
 
 public class WordCount {
 
     public static void main(String[] args) {
-
-        Scanner userInput = new Scanner(System.in);
-
-        String potentialWords = userInput.nextLine();
-
+        int wordCount = 0;
         System.out.print("Enter text:");
+        Scanner userInput = new Scanner(System.in);
+        String inputString = userInput.nextLine();
 
+        List<String> potentialWords = Arrays.asList(inputString.split("\\s"));
+
+        for (String word : potentialWords) {
+            if (!word.isEmpty()) {
+                wordCount++;
+            }
+        }
+
+        System.out.println("Number of words: " + wordCount);
     }
 }
