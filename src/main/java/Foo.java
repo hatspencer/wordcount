dd7ca364b1de4b5fcafeb153806d3a09e707ba78
Michal Somorovsky <>
2020-03-19 09:59:08 +0100

    WIP

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class Foo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter text: ");
+        String text = scanner.nextLine();
+
+        String[] words = text.trim().split("\\s+");
+
+        Pattern pattern = Pattern.compile("[a-zA-Z]");
+
+        List<String> matching = Arrays.stream(words).filter(pattern.asPredicate()).collect(Collectors.toList());
+
+        System.out.println(matching);
+
+    }
 }
