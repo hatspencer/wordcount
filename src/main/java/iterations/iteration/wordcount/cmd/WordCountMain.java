a286f6d9645b82d3b14f88730073670fc26906c3
juraj <juraj@jurko>
2020-03-20 09:57:20 +0100

    JuBar: Initialize stop words from file in UI controller

--- src/main/java/iterations/iteration/wordcount/cmd/WordCountMain.java
+++ src/main/java/iterations/iteration/wordcount/cmd/WordCountMain.java
@@ -1,6 +1,9 @@
 package iterations.iteration.wordcount.cmd;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,8 +11,11 @@ import iterations.iteration.wordcount.WordCount;
 
 public class WordCountMain {
 
+	private static final String STOP_WORDS_DEFAULT_LOCATION = "stopwords.txt";
+
 	public static void main(String[] args) {
 		WordCount wordCount = new WordCount();
+		initStopWords(wordCount, STOP_WORDS_DEFAULT_LOCATION);
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -21,6 +27,34 @@ public class WordCountMain {
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println("Reading your command line input failed");
+		}
+	}
+	
+	private static void initStopWords(WordCount wordCount, String stopWordsFileName) {
+		File stopWordsFile = new File(stopWordsFileName);
+		if (stopWordsFile.exists()) {
+			FileInputStream inputFileStream = null;
+			try {
+				inputFileStream = new FileInputStream(stopWordsFile);
+				BufferedReader stopWordsReader = new BufferedReader(new InputStreamReader(inputFileStream));
+				String stopWord;
+				while((stopWord = stopWordsReader.readLine()) != null) {
+					wordCount.addStopWords(stopWord);
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			finally {
+				if (inputFileStream != null) {
+					try {
+						inputFileStream.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 	}
 
