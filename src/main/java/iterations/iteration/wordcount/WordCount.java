255708cb920c16a431218c784da4905c5f03bd9c
juraj <juraj@jurko>
2020-03-20 11:01:20 +0100

    JuBar: refactor counting words - collect them into a list and then return the size

--- src/main/java/iterations/iteration/wordcount/WordCount.java
+++ src/main/java/iterations/iteration/wordcount/WordCount.java
@@ -2,7 +2,10 @@ package iterations.iteration.wordcount;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class WordCount {
@@ -10,16 +13,20 @@ public class WordCount {
 	final Set<String> stopWords = new HashSet<String>();
 
 	public int countWords(String input) {
+		return collectValidWords(input).size();
+	}
+	
+	protected List<String> collectValidWords(String input) {
 		if (input == null)
-			return 0;
+			return Collections.emptyList();
 		String[] inputWords = input.split("[ ,\\t]+");
-		int count = 0;
+		List<String> validWords = new LinkedList<String>();
 		for (int i = 0; i < inputWords.length; i++) {
 			if (isValidWord(inputWords[i])) {
-				count++;
+				validWords.add(inputWords[i]);
 			}
 		}
-		return count;
+		return validWords;
 	}
 	
 	public void addStopWords(String... stopWords) {
