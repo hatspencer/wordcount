8e3749b7f7188c0d0de4b430ccf9ff2273951901
alerossi82 <alerossi82@hotmail.com>
2020-04-16 10:01:44 +0200

    - first session

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,9 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Foo {
 
-  //console input
+  public String inputText() throws IOException {
+    System.out.println("Enter text: ");
+    BufferedReader reader =
+        new BufferedReader(new InputStreamReader(System.in));
+     return reader.readLine();
+  }
   
   public int countWords(String text) {
     String[] words = text.split(" ");
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,13 +1,15 @@
+import java.io.IOException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class FooTest {
 
   @Test
-  public void splitterTest(){
-    String str = "word wor4r 75";
+  public void splitterTest() throws IOException {
+//    String str = "word wor4r 75";
     Foo foo = new Foo();
-    int result = foo.countWords(str);
+    String text = foo.inputText();
+    int result = foo.countWords(text);
     Assert.assertEquals(result, 1);
   }
 
