ee7feca1646b2f1bcffa43d0e711611d4f98a832
alerossi82 <alerossi82@hotmail.com>
2020-04-16 09:54:27 +0200

    - WIP

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Foo {
+
+  //console input
+  
+  public int countWords(String text) {
+    String[] words = text.split(" ");
+    List<String> validWords = wordsFilter(words);
+    return validWords.size();
+  }
+
+  private List<String> wordsFilter(String[] words) {
+    List<String> validWords = new ArrayList<String>();
+    for (String s : words) {
+      if (s.matches("[a-zA-Z]+")) {
+        validWords.add(s);
+      }
+    }
+    return validWords;
+  }
+
+  //output to terminal
 }
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FooTest {
+
+  @Test
+  public void splitterTest(){
+    String str = "word wor4r 75";
+    Foo foo = new Foo();
+    int result = foo.countWords(str);
+    Assert.assertEquals(result, 1);
+  }
+
 }
