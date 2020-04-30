5a21b6833a3c6c75268389ad7ac9d89ca8c6f4c2
unknown <mario.lenicky@gmail.com>
2020-04-30 11:00:59 +0200

    file with stop words

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Foo {
 
+    private static final Set<String> STOP_WORDS = new HashSet<>();
+    private static final String STOP_WORDS_FILENAME = "src/main/resources/stopwords.txt";
+
     public static void main(String[] args) {
+
+        readStopWords(STOP_WORDS);
 
         System.out.print("Enter text: ");
 
@@ -11,16 +20,22 @@ public class Foo {
 
         String sentence = scanner.nextLine();
 
-        System.out.println("Number of words: " + countWords(sentence));
+        System.out.println("Number of words: " + countWords(sentence, STOP_WORDS));
 
         scanner.close();
     }
 
-    public static long countWords(String sentence) {
+    public static long countWords(String sentence, Set<String> stopWords) {
+
+        if (sentence == null) {
+            sentence = "";
+        }
+
         String[] words = sentence.split(" ");
 
         long numWords = Arrays.stream(words)
                 .filter(Foo::isWord)
+                .filter(s -> stopWords == null || !stopWords.contains(s))
                 .count();
 
         return numWords;
@@ -32,5 +47,21 @@ public class Foo {
         }
         return word.replaceAll("[a-zA-Z]", "").length() == 0;
     }
+
+    private static void readStopWords(Set<String> stopWords) {
+        try {
+            File myObj = new File(STOP_WORDS_FILENAME);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                stopWords.add(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            e.printStackTrace();
+        }
+    }
+
 
 }
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,8 +1,13 @@
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 
 public class FooTest {
+
+    private static final Set<String> STOP_WORDS = new HashSet<>();
 
     @Test
     public void wordValidityTest() {
@@ -21,10 +26,10 @@ public class FooTest {
     @Test
     public void wordCountTest() {
 
-        assertEquals(1, Foo.countWords("word"));
-        assertEquals(2, Foo.countWords("word word"));
-        assertEquals(2, Foo.countWords("  word    6fdf  uionj"));
-        assertEquals(0, Foo.countWords("   "));
+        assertEquals(1, Foo.countWords("word", STOP_WORDS));
+        assertEquals(2, Foo.countWords("word word", STOP_WORDS));
+        assertEquals(2, Foo.countWords("  word    6fdf  uionj", STOP_WORDS));
+        assertEquals(0, Foo.countWords("   ", STOP_WORDS));
 
     }
 
