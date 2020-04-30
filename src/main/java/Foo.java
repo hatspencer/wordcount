c375d6a94031d625d454467ad48d288ce99be0d4
unknown <mario.lenicky@gmail.com>
2020-04-30 10:02:49 +0200

    word counter - initial commit

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,7 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Foo {
 
+    public static void main(String[] args) {
 
-    private int a;
+        System.out.println("Enter text: ");
 
+        Scanner scanner = new Scanner(System.in);
+
+        String sentence = scanner.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        long numWords = Arrays.stream(words)
+                .filter(Foo::isWord)
+                .count();
+
+        System.out.println("Number of words: " + numWords);
+
+        scanner.close();
+    }
+
+    private static boolean isWord(String word) {
+        if (word == null || word.trim().isEmpty()) {
+            return false;
+        }
+        return word.replaceAll("[a-zA-Z]", "").length() == 0;
+    }
 
 }
