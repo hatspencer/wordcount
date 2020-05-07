42299f186ba9f5a1bb23416c75edbf5e4f84bb3f
jakubtutko <jakub.tutko@zentity.com>
2020-05-07 10:29:01 +0200

    Reflect package tree in tests

--- src/test/java/word/count/impl/WordCounterImplTest.java
+++ src/test/java/word/count/impl/WordCounterImplTest.java
@@ -1,3 +1,5 @@
+package word.count.impl;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
--- src/test/java/word/match/impl/AzWordMatcherImplTest.java
+++ src/test/java/word/match/impl/AzWordMatcherImplTest.java
@@ -1,3 +1,5 @@
+package word.match.impl;
+
 import static org.junit.Assert.*;
 
 import org.junit.Test;
