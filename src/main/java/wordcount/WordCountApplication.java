5962ffc68154613cd0c20d1ecc86c177d7e44360
PC <PC@DESKTOP-O38C2IT.localdomain>
2020-05-13 02:42:46 -0700

    WordCount - iteration3 (Christian)

--- src/main/java/wordcount/WordCountApplication.java
+++ src/main/java/wordcount/WordCountApplication.java
@@ -10,15 +10,31 @@ import wordcount.service.WordCountService;
 public class WordCountApplication {
 
 	public static void main(String[] args) throws Exception {
-		if (args.length > 0 && Files.isReadable(Paths.get(args[0]))) {
-			String filename = args[0];
-			long wordCount = WordCountService.getWordCountFromFile(filename);
-			System.out.println("Number of words: " + wordCount);
+		if (args.length > 0 && fileIsReadable(args[0])) {
+			getWordCountFromFile(args[0]);
 		} else {
-			System.out.println("Enter text:");
-			String input = new BufferedReader(new InputStreamReader(System.in)).readLine();
-			System.out.println("Number of words: " + WordCountService.getWordCount(input));
+			getWordCountFromStandardInput();
 		}
 	}
 
+	public static final boolean fileIsReadable(String input) {
+		return Files.isReadable(Paths.get(input));
+	}
+	
+	public static final void getWordCountFromFile(String filename) {
+		long wordCount = WordCountService.getWordCountFromFile(filename);
+		System.out.println("Number of words: " + wordCount);
+	}
+	
+	public static final void getWordCountFromStandardInput() {
+		System.out.println("Enter text:");
+		try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+			String input = reader.readLine();
+			System.out.println("Number of words: " + WordCountService.getWordCount(input));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
 }
