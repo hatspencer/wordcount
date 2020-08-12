e9694e24c93adae7157850499188a24c62a1029f
DESKTOP-OCCG6UN\Markovi <pav.marek80@gmail.com>
2020-08-12 15:59:47 +0200

    Refactored test class

--- src/test/java/wordcount/WordCountTest.java
+++ src/test/java/wordcount/WordCountTest.java
@@ -18,29 +18,44 @@ import java.net.URISyntaxException;
 import static org.junit.Assert.*;
 
 public class WordCountTest {
-    
+
+    private StopWords stopWords = readStopWords();
+
     @Test
-    public void splitterTest() {
-       String[] twoWords = { "aaa", "bbb" };
+    public void splitterTest1() {
+        String[] twoWords = {"aaa", "bbb"};
         Assert.assertArrayEquals(Splitter.split("aaa bbb"), twoWords);
-        
+    }
+
+    @Test
+    public void splitterTest2() {
        String[] emptyInput = {  };
         Assert.assertArrayEquals(Splitter.split(""), emptyInput);
+    }
 
-       String[] oneWordOneBadword = { "aaa", "bb" };
+    @Test
+    public void splitterTest3() {
+        String[] oneWordOneBadword = { "aaa", "bb" };
         Assert.assertArrayEquals(Splitter.split("aaa bb."), oneWordOneBadword);
+    }
 
+    @Test
+    public void splitterTest4() {
         String[] delimiterWhitespaceAndDot = { "aaa", "bb", "cc" };
         Assert.assertArrayEquals(Splitter.split("aaa. bb.cc."), delimiterWhitespaceAndDot);
+    }
 
+    @Test
+    public void splitterTest5() {
         String[] twoWordsExtraSpaces = { "aaa", "bb" };
         Assert.assertArrayEquals(Splitter.split("aaa       bb"), twoWordsExtraSpaces);
+    }
 
+    @Test
+    public void splitterTest6() {
         String[] tabDelimiter = { "aaa", "bb" };
         Assert.assertArrayEquals(Splitter.split("aaa"+((char)9)+"bb"), tabDelimiter);
-
-
-    }    
+    }
 
     private void wordCounterTest(String[] array, StopWords stopWords, int wordCount, int unique) {
         Statistic statistic = WordCounter.getWordsCount(array, stopWords);
@@ -49,37 +64,56 @@ public class WordCountTest {
     }
 
     @Test
-    public void wordCounterTests() {
-       String[] nullArray = null;
-       
-       StopWords stopWords = readStopWords();
-
+    public void wordCounterTests1() {
+        String[] nullArray = null;
         wordCounterTest(nullArray, stopWords, 0, 0);
-        
+    }
+
+    @Test
+    public void wordCounterTests2() {
         String[] emptyArray = {  };
         wordCounterTest(emptyArray, stopWords, 0, 0);
+    }
 
-       String[] twoWords = { "aa", "BB" };
+    @Test
+    public void wordCounterTests3() {
+        String[] twoWords = { "aa", "BB" };
         wordCounterTest(twoWords, stopWords, 2, 2);
-       
-       String[] towWordsWithOneBad = { "aa", "bb.", "CC" };
+    }
+
+    @Test
+    public void wordCounterTests4() {
+        String[] towWordsWithOneBad = { "aa", "bb.", "CC" };
         wordCounterTest(towWordsWithOneBad, stopWords, 2, 2);
-        
-       String[] onlyBadWords = { "aa.", "pavel1marek", "koník" };
+    }
+
+    @Test
+    public void wordCounterTests5() {
+        String[] onlyBadWords = { "aa.", "pavel1marek", "koník" };
         wordCounterTest(onlyBadWords, stopWords, 0, 0);
+    }
 
-       String[] textWithStopWords = { "aa.", "pavel1marek", "on", "word", "a" };
-        wordCounterTest(textWithStopWords, stopWords, 1, 1);
+    @Test
+    public void wordCounterTests6() {
+        String[] textWithStopWords = { "aa.", "pavel1marek", "on", "word", "a" };
+    }
 
-       String[] onlyStopWords = { "on", "a" };
+    @Test
+    public void wordCounterTests7() {
+        String[] onlyStopWords = { "on", "a" };
         wordCounterTest(onlyStopWords, stopWords, 0, 0);
+    }
 
+    @Test
+    public void wordCounterTests8() {
         String[] iteration4Test = { "Humpty", "Dumpty", "sat", "on", "a", "all", "Humpty", "Dumpty", "had", "a", "great", "fall" };
         wordCounterTest(iteration4Test, stopWords, 9, 7);
+    }
 
+    @Test
+    public void wordCounterTests9() {
         String[] duplicates = { "duplicate", "words", "duplicate", "duplicate", "duplicate" };
         wordCounterTest(duplicates, stopWords, 5, 2);
-
     }
 
     private StopWords readStopWords() {
@@ -116,11 +150,17 @@ public class WordCountTest {
     }
 
     @Test
-    public void fileInputTest() {
+    public void fileInputTest1() {
         testFileInputCatchException("This is small test", "ThisIsSmallTest.txt");
+    }
 
+    @Test
+    public void fileInputTest2() {
         testFileInputCatchException("This is multiline test", "ThisIsMultilineTest.txt");
+    }
 
+    @Test
+    public void fileInputTestNotExistiting() {
         assertThrows(IOException.class, () -> {
             String a = readFileInput("FileNotExists.txt");
         });
@@ -132,13 +172,17 @@ public class WordCountTest {
     }
 
     @Test
-    public void testEndToEnd() {
-        StopWords stopWords = readStopWords();
-
+    public void testEndToEnd1() {
         testFromInputToStats("Mary had a little lamb", 4, 4, stopWords);
+    }
 
+    @Test
+    public void testEndToEnd2() {
         testFromInputToStats("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.", 9, 7, stopWords);
+    }
 
+    @Test
+    public void testEndToEnd3() {
         testFromInputToStats("S0me we1rd w0rds are in input text. on a I don't know text.", 7, 6, stopWords);
     }
 }
