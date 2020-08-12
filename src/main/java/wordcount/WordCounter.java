60ab4e83e1110e860617f1894663c90f5cc8d20c
DESKTOP-OCCG6UN\Markovi <pav.marek80@gmail.com>
2020-08-12 11:01:05 +0200

    Added splitter and wordcounter implementation

--- src/main/java/wordcount/WordCounter.java
+++ src/main/java/wordcount/WordCounter.java
@@ -1,2 +1,24 @@
-package wordcount;public class WordCounter {
+package wordcount;
+
+import java.util.regex.Pattern;
+
+public class WordCounter {
+
+    public static int getWordsCount(String [] inputArray) {
+        int words = 0;
+
+        if (inputArray == null) {
+            return 0;
+        }
+
+        for(String word : inputArray ) {
+            System.out.println("("+word+")");
+            if (Pattern.compile("^[a-zA-Z]+$").matcher(word).find()) {
+                words = words + 1;
+            }
+        }
+
+        return words;
+    }
+
 }
