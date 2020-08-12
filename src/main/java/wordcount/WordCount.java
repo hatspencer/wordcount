749ee2af6a0361510aff50106004987c89ccbabf
DESKTOP-OCCG6UN\Markovi <pav.marek80@gmail.com>
2020-08-12 11:00:42 +0200

    Added splitter and wordcounter implementation

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,2 +1,16 @@
-public class Foo {
+package wordcount;
+
+public class WordCount {
+    
+    public static void main(String[] args) {
+        
+        String input = UserInput.getInputFromUser();
+        
+        String[] splittedInput = Splitter.split(input);
+        
+//        System.out.println("debug "+input);
+        System.out.println("Number of words: " + WordCounter.getWordsCount(splittedInput));
+
+    }
+     
 }
--- src/test/java/wordcount/WordCountTest.java
+++ src/test/java/wordcount/WordCountTest.java
@@ -1,2 +1,56 @@
-public class FooTest {
+package wordcount;
+
+import org.junit.Assert;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class WordCountTest {
+    
+    private boolean checkEquality(String[] s1, String[] s2) {
+		if (s1 == s2)
+			return true;
+
+		if (s1 == null || s2 == null)
+			return false;
+
+		int n = s1.length;
+		if (n != s2.length)
+			return false;
+
+		for (int i = 0; i < n; i++) {
+			if (!s1[i].equals(s2[i]))
+				return false;
+		}
+
+		return true;
+	}
+    
+    @Test
+    public void splitterTest() {
+       String[] s1 = { "aaa", "bbb" };
+       assertTrue(checkEquality(Splitter.split("aaa bbb"), s1));
+        
+       String[] s2 = {  };
+       assertTrue(checkEquality(Splitter.split(""), s2));
+
+       String[] s3 = { "aaa", "bb." };
+       assertTrue(checkEquality(Splitter.split("aaa bb."), s3));
+       
+
+    }    
+
+    @Test
+    public void wordCounterTest() {
+       String[] s1 = {  };
+       Assert.assertEquals(WordCounter.getWordsCount(s1), 0);
+       
+       String[] s2 = { "aa", "BB" };
+       Assert.assertEquals(WordCounter.getWordsCount(s2), 2);
+       
+       String[] s3 = { "aa", "bb.", "CC" };
+       Assert.assertEquals(WordCounter.getWordsCount(s3), 2);
+        
+
+    }    
+    
 }
