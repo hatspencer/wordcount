4671895618360f1f9cfbf589f00c67b089ec9a7d
jhope <not@mail.com>
2020-08-17 11:41:02 +0200

    Implement method count2(String)

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,23 +1,65 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public final class WordCounter {
 
-    private final static Pattern WORDS_PATTERN = Pattern.compile("([^a-zA-Z])");
+    private static final Pattern WORDS_PATTERN = Pattern.compile("([^a-zA-Z])");
+    private static final String STOPWORDS_FILE_NAME = "stopwords.txt";
 
     private final List<String> forbiddenWords = new ArrayList<>();
 
     public int count(final String sentence) {
-        if (sentence == null || sentence.isEmpty()) {
+        if (isInvalidSentence(sentence)) {
             return 0;
         }
 
-        return WORDS_PATTERN.split(sentence).length;
+        return splitSentence(sentence).length;
     }
 
-    public int count2(final String sentence) {
-        return 0;
+    public long count2(final String sentence) {
+        if (isInvalidSentence(sentence)) {
+            return 0;
+        }
+
+        if (forbiddenWords.isEmpty()) {
+            readForbiddenWords();
+        }
+
+        final String[] resultSentence = splitSentence(sentence);
+
+        return Arrays.stream(resultSentence).filter(it -> !forbiddenWords.contains(it)).count();
+    }
+
+    private void readForbiddenWords() {
+        final URL resourceURL = WordCounter.class.getResource(STOPWORDS_FILE_NAME);
+
+        try {
+            final Path stopWordFile = Paths.get(resourceURL.toURI());
+            final List<String> lines = Files.readAllLines(stopWordFile, StandardCharsets.UTF_8);
+
+            forbiddenWords.clear();
+            forbiddenWords.addAll(lines);
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static String[] splitSentence(final String sentence) {
+        return WORDS_PATTERN.split(sentence);
+    }
+
+    private static boolean isInvalidSentence(final String sentence) {
+        return sentence == null || sentence.isEmpty();
     }
 
 }
