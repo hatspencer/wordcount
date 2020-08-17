72c20d2850e1572838586148a6d0a799dbdbf991
jhope <not@mail.com>
2020-08-17 10:32:47 +0200

    Add and implement count(int) method

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,15 @@
+import java.util.regex.Pattern;
+
 public class Foo {
+
+    private final static Pattern WORDS_PATTERN = Pattern.compile("([^a-zA-Z])");
+
+    public int count(final String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return 0;
+        }
+
+        return WORDS_PATTERN.split(sentence).length;
+    }
+
 }
