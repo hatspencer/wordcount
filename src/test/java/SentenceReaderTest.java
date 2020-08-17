d68578de3b0294d89eae37619717d84550d028c8
jhope <not@mail.com>
2020-08-17 14:54:13 +0200

    Add should_not_read_file_if_not_exists test

--- src/test/java/SentenceReaderTest.java
+++ src/test/java/SentenceReaderTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,7 +18,23 @@ public class SentenceReaderTest {
         final String sentence = sentenceReader.read(args);
 
         // then
-        assertEquals(5, sentence.length());
+        assertEquals(22, sentence.length());
+    }
+
+    @Test
+    public void should_not_read_file_if_not_exists() throws IOException {
+        // given
+        final SentenceReader sentenceReader = new SentenceReader();
+        final String[] args = new String[0];
+
+        // when
+        final InputStream in = new ByteArrayInputStream("Mary had a little lamb".getBytes());
+        System.setIn(in);
+        final String sentence = sentenceReader.read(args);
+        System.setIn(System.in);
+
+        // then
+        assertEquals(22, sentence.length());
     }
 
 }
