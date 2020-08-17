f4cc3c2438e4e172d3c14acf11472cbeedbe12e3
jhope <not@mail.com>
2020-08-17 10:14:35 +0200

    Make test parametrized

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -2,6 +2,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -14,12 +17,30 @@ public class FooTest {
 //            "" 0
 //            "word Word" 2
 
+    private final String sentence;
+    private final int count;
+
+    public FooTest(String sentence, int count) {
+        this.sentence = sentence;
+        this.count = count;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> inputData() {
+        return Arrays.asList(new Object[][]{
+                {"word", 1},
+                {"word word", 2},
+                {"wo2rd", 2},
+                {"", 0},
+                {"word Word", 2}
+        });
+    }
 
     @Test
     public void should_count_words() {
         // given
-        final int expectedWordNumber = 1;
-        final String testSentence = "word";
+        final int expectedWordNumber = count;
+        final String testSentence = sentence;
 
         // when
         final String[] words = testSentence.split("[^a-zA-Z]");
