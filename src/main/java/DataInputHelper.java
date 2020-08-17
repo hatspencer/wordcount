0f68b51802893af552e1cf805c652f87c5d85167
tkowalski <tomasz.r.kowalski@gmail.com>
2020-08-17 11:57:22 +0200

    iteration 3

--- src/main/java/DataInputHelper.java
+++ src/main/java/DataInputHelper.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class DataInputHelper {
-    public List<String> readFile() {
+    public List<String> readStopWordsFile() {
         List<String> stopWordsList = new ArrayList<>();
         try {
             stopWordsList = Files.readAllLines(new File("stopWords.txt").toPath());
@@ -16,7 +16,23 @@ public class DataInputHelper {
         return stopWordsList;
     }
 
-    public String readUserInput() {
+    public String readWordsToCount(String filename) {
+        String wordsToCount = null;
+        if (filename != null) {
+            List<String> wordsToCountList;
+            try {
+                wordsToCountList = Files.readAllLines(new File(filename).toPath());
+                wordsToCount = String.join(" ", wordsToCountList);
+            } catch (IOException e) {
+                System.out.println("Error reading file with words to count: " + e);
+            }
+        } else {
+            wordsToCount = readUserInput();
+        }
+        return wordsToCount;
+    }
+
+    private String readUserInput() {
         System.out.println("Enter text: ");
         Scanner userInputScanner = new Scanner(System.in);
         return userInputScanner.nextLine();
--- src/main/java/WordApp.java
+++ src/main/java/WordApp.java
@@ -6,8 +6,16 @@ public class WordApp {
     private static DataInputHelper dataInputHelper = new DataInputHelper();
 
     public static void main(String... args) {
-        List<String> stopWordLines = dataInputHelper.readFile();
-        String userInput = dataInputHelper.readUserInput();
-        System.out.println("Number of words: " + wordsCounter.countWords(userInput, stopWordLines));
+        String filename = null;
+        if (args.length > 0) {
+            filename = args[0];
+        }
+        List<String> stopWordLines = dataInputHelper.readStopWordsFile();
+        String wordsToCount = dataInputHelper.readWordsToCount(filename);
+        System.out.println("Number of words: " + wordsCounter.countWords(wordsToCount, stopWordLines));
+    }
+
+    private static boolean isFilenameProvided(String... args) {
+        return args.length > 0;
     }
 }
