1d277ed2927a4fd2976e1705d7918c48a29e77c6
jhope <not@mail.com>
2020-08-17 13:35:31 +0200

    Read data from file when given single parameter. Read data set from console if zero parameters.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,27 +1,54 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
-public class App {
+public final class App {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) throws IOException {
 
-        final Scanner in = new Scanner(System.in);
         final WordCounter wordCounter = new WordCounter();
+        final Scanner in = new Scanner(System.in);
+        final boolean isNoParameter = args == null || args.length == 0;
 
-        if (in.hasNextLine()) {
-            final String firstLine = in.nextLine();
+        if (isNoParameter) {
 
-            if (isInvalidFile(firstLine)) {
-
+            while (in.hasNextLine()) {
+                final String line = in.nextLine();
+                countWords(wordCounter, line);
             }
 
-            final int count = wordCounter.count(firstLine);
-            System.out.println("Count: " + count);
+        } else if (args.length == 1) {
+            final String fileName = args[0];
+
+            if (!isFileName(fileName)) {
+                throw new NoSuchFileException("No file: " + fileName);
+            }
+
+            final Path dataFile = Paths.get(fileName);
+            final List<String> lines = Files.readAllLines(dataFile, StandardCharsets.UTF_8);
+            final String fileWords = String.join(" ", lines);
+
+            countWords(wordCounter, fileWords);
         }
 
     }
 
-    private static boolean isInvalidFile(final String firstLine) {
-        return false;
+    private static void countWords(final WordCounter wordCounter, final String line) {
+        final long count = wordCounter.count2(line);
+        System.out.println("Count: " + count);
+    }
+
+    private static boolean isFileName(final String firstLine) {
+        final Path path = Paths.get(firstLine);
+        final File file = path.toFile();
+
+        return file.exists() && !file.isDirectory();
     }
 
 }
