3e5da34501538446f32269aa16da595aa66ce115
jhope <not@mail.com>
2020-08-17 09:57:55 +0200

    Add first test to FooTest

--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -1,2 +1,32 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Parameterized.class)
 public class FooTest {
+
+//            "word" 1
+//            "word word" 2
+//            "wo2rd" 2
+//            "" 0
+//            "word Word" 2
+
+
+    @Test
+    public void should_count_words() {
+        // given
+        final int expectedWordNumber = 1;
+        final String testSentence = "word";
+
+        // when
+        final String[] words = testSentence.split("[^a-zA-Z]");
+
+        // then
+        assertNotNull(words);
+        assertEquals(expectedWordNumber, words.length);
+    }
+
 }
