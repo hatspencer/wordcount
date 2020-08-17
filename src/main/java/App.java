21607252262ab2f09574644f0f32b8074a65631f
jhope <not@mail.com>
2020-08-17 11:54:00 +0200

    Implement main(String[]) method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+
+        final Scanner in = new Scanner(System.in);
+        final WordCounter wordCounter = new WordCounter();
+
+        while (in.hasNextLine()) {
+            final String line = in.nextLine();
+            final int count = wordCounter.count(line);
+            System.out.println("Count: " + count);
+        }
 
     }
 
