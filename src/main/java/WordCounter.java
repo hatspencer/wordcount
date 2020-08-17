5a5c76477d13e05210926b0ad855ada268baa840
jhope <not@mail.com>
2020-08-17 10:55:59 +0200

    Add count2 method

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public final class WordCounter {
 
     private final static Pattern WORDS_PATTERN = Pattern.compile("([^a-zA-Z])");
+
+    private final List<String> forbiddenWords = new ArrayList<>();
 
     public int count(final String sentence) {
         if (sentence == null || sentence.isEmpty()) {
@@ -10,6 +14,10 @@ public final class WordCounter {
         }
 
         return WORDS_PATTERN.split(sentence).length;
+    }
+
+    public int count2(final String sentence) {
+        return 0;
     }
 
 }
