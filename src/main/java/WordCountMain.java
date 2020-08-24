1bb342bddad7461858c58c43d45388b25e64739e
Heinrich Nirschl <Heinrich.Nirschl@extern.s-itsolutions.at>
2020-08-24 10:16:07 +0200

    added list of stop words

--- src/main/java/WordCountMain.java
+++ src/main/java/WordCountMain.java
@@ -1,21 +1,39 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class WordCountMain {
 
+    List<String> stopwords;
+
+    public WordCountMain() throws IOException {
+        readStopwords("stopwords.txt");
+    }
+
     public static void main(String[] args) throws IOException {
+        WordCountMain app = new WordCountMain();
         System.out.print("Enter text: ");
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         String line = input.readLine();
-        System.out.printf("Number of words %d%n", wordCount(line));
+        System.out.printf("Number of words %d%n", app.wordCount(line));
     }
 
-    static int wordCount(String line) {
+    void readStopwords(String filename) throws IOException {
+        stopwords = new ArrayList<>();
+        File stopwordsFile = new File(filename);
+        BufferedReader input = new BufferedReader(new FileReader(stopwordsFile));
+        String stopword;
+        while ((stopword = input.readLine()) != null) {
+            stopwords.add(stopword);
+        }
+        input.close();
+    }
+
+    int wordCount(String line) {
         String[] parts = line.split(" ");
         int count = 0;
         for (String part : parts) {
-            if (part.matches("[a-zA-Z]+")) {
+            if (part.matches("[a-zA-Z]+") && !stopwords.contains(part)) {
                 count++;
             }
         }
--- src/test/java/WordCountMainTest.java
+++ src/test/java/WordCountMainTest.java
@@ -1,21 +1,36 @@
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class WordCountMainTest {
 
     @Test
-    public void testSingleWord() {
-        assertEquals(1, WordCountMain.wordCount("word"));
+    public void testSingleWord() throws IOException {
+        WordCountMain app = new WordCountMain();
+        assertEquals(1, app.wordCount("word"));
     }
 
     @Test
-    public void testNotSingleWord() {
-        assertEquals(0, WordCountMain.wordCount("word2"));
+    public void testNotSingleWord() throws IOException {
+        WordCountMain app = new WordCountMain();
+        assertEquals(0, app.wordCount("word2"));
     }
 
     @Test
-    public void testSeveralWords() {
-        assertEquals(5, WordCountMain.wordCount("Mary had a little lamb"));
+    public void testSeveralWords() throws IOException {
+        WordCountMain app = new WordCountMain();
+        assertEquals(4, app.wordCount("Mary had a little lamb"));
+    }
+
+    @Test
+    public void testReadStopwords() throws IOException {
+        WordCountMain app = new WordCountMain();
+        assertTrue(app.stopwords.contains("the"));
+        assertTrue(app.stopwords.contains("a"));
+        assertTrue(app.stopwords.contains("on"));
+        assertTrue(app.stopwords.contains("off"));
     }
 }
