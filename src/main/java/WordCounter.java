07295e3285c54da9966290186e3ef94a05134496
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 16:24:42 +0200

    Hyphen words should be one word

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -7,11 +7,9 @@ import java.util.stream.Collectors;
 public class WordCounter {
 
     public long countValidWords(String input) {
-        List<String> wordList = Arrays.asList(input.trim().split("\\s+"));
+        List<String> wordList = Arrays.asList(sanitizeString(input).split("\\s+"));
         return wordList.stream()
                 .filter(word ->  word.matches( "[a-zA-Z]+" ))
-                .map(word -> word.replace(".", ""))
-                .map(word -> word.replace("-", " "))
                 .count();
     }
 
@@ -23,15 +21,13 @@ public class WordCounter {
     }
 
     public long countUniqueWords(String input, List<String> stopWords) {
-        List<String> wordList = Arrays.asList(sanitizeString(input).split("\\s+"));
-        List<String> santizedList =  wordList.stream()
+        List<String> santizedList =  Arrays.asList(sanitizeString(input).split("\\s+")).stream()
                 .filter(word -> !stopWords.contains(word))
                 .filter(word -> word.matches("[a-zA-Z]+")).collect(Collectors.toList());
-        Set<String> set = new HashSet<>(santizedList);
-        return set.size();
+        return new HashSet<>(santizedList).size();
     }
 
     private String sanitizeString(String input) {
-        return input.replace(".", "").replace("-", " ");
+        return input.replace(".", "").replace("-", "");
     }
 }
