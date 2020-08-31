ea51e485f3df956b19af656c209341612209e681
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-08-31 11:45:39 +0200

    Iteration 2 - add read stop words from file

--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,15 +1,34 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Foo {
 
+    private List<String> stopWords;
+
     public static void main(String[] args) {
+        new Foo().readStopWords("./src/main/resources/stopwords.txt");
         System.out.println("Enter text: ");
         Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
         int wordCount = new Foo().getWordCount(text);
-
         System.out.println("Number of words: " + wordCount);
+    }
+
+    public void readStopWords(String fileName) {
+        stopWords = new ArrayList<>();
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stopWords.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getWordCount(String text) {
