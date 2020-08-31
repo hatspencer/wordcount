05c228cf319c9e9a00649087112a1b5edcdd1c1a
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 10:49:47 +0200

    naive solution

--- src/main/java/WordCount.java
+++ src/main/java/WordCount.java
@@ -1,29 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 public class WordCount {
 
     public static void main(String[] args) {
 
-        // input reader
-        // store input to string
-        // analyse text
-            // while loop
-            // regex
-        // count words
-        //Enter data using BufferReader
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(System.in));
 
-        // Reading data using readLine
-
         try {
-            String name = reader.readLine();
-            System.out.println(name);
-
+            System.out.println("Enter Text: ");
+            String userInput = reader.readLine();
+            System.out.println("Number of words: " + countWordsReturnNumberOfValidWords(userInput));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Could not process request, please try again");
         }
     }
+
+    // valid word constitutes "[a-zA-Z]+"
+    private static long countWordsReturnNumberOfValidWords(String name) {
+        List<String> wordList = Arrays.asList(name.trim().split("\\s+"));
+        return wordList.stream().filter(word ->  word.matches( "[a-zA-Z]+" )).count();
+    }
+
 }
