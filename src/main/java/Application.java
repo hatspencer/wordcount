b2017b1696adcdea46a4a1511a89f7d3546b8b10
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 12:02:40 +0200

    use iteration 2

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,20 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        WordCounter wc = new WordCounter();
 
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(System.in));
 
-        WordCounter wc = new WordCounter();
+        List<String> lines = Files.readAllLines(Paths.get("stopwords.txt"), StandardCharsets.UTF_8);
 
         try {
             System.out.println("Enter Text: ");
-            System.out.println("Number of words: " + wc.countValidWords(reader.readLine()));
-
+            // System.out.println("Number of words: " + wc.countValidWords(reader.readLine()));
+            System.out.println("Number of words (when processed against stop words): " + wc.countValidWords(reader.readLine(), lines));
         } catch (IOException e) {
             System.out.println("Could not process request, please try again");
         }
