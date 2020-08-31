50fc47bb5f2617e8a4e2ab09d8e1a58ee63e4324
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-08-31 11:58:29 +0200

    Iteration 2 - add stop words feature (WIP)

---
+++ src/main/java/WordCountExample.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class WordCountExample {
+
+    private List<String> stopWords = new ArrayList<>();
+
+    public static void main(String[] args) {
+        WordCountExample foo = new WordCountExample();
+        foo.readStopWords("./src/main/resources/stopwords.txt");
+        System.out.println("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int wordCount = foo.getWordCount(text);
+        System.out.println("Number of words: " + wordCount);
+        int wordCountWithoutStopWords = foo.getWordCountWithoutStopWords(text);
+        System.out.println("Number of words without stop words: " + wordCountWithoutStopWords);
+    }
+
+    public void readStopWords(String fileName) {
+        BufferedReader bufferedReader;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                stopWords.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getWordCount(String text) {
+        int wordCount = 0;
+        String trim = text.trim();
+        if (trim.isEmpty()) {
+            return 0;
+        }
+        String[] split = trim.split("\\s+");
+        for (String s : split) {
+            boolean matches = Pattern.matches("[a-zA-Z]*", s);
+            if (matches) {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+
+    boolean isStopWord(String word) {
+        for (String stopWord : stopWords) {
+            if (word.contentEquals(stopWord)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getWordCountWithoutStopWords(String text) {
+        int wordCount = 0;
+        String trim = text.trim();
+        if (trim.isEmpty()) {
+            return 0;
+        }
+        String[] split = trim.split("\\s+");
+        for (String s : split) {
+            boolean matches = Pattern.matches("[a-zA-Z]*", s);
+            if (matches && !isStopWord(s)) {
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+
+}
--- src/test/java/FooTest.java
+++ src/test/java/FooTest.java
@@ -5,26 +5,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FooTest {
     @Test
     void TestWithFiveWords() {
-        assertEquals(5, new Foo().getWordCount("Mary had a little lamb"));
+        assertEquals(5, new WordCountExample().getWordCount("Mary had a little lamb"));
     }
 
     @Test
     void TestWithFiveWordsAndSpecialCharacter() {
-        assertEquals(4, new Foo().getWordCount("Mary had a little lamb."));
+        assertEquals(4, new WordCountExample().getWordCount("Mary had a little lamb."));
     }
 
     @Test
     void TestWithEmptyString() {
-        assertEquals(0, new Foo().getWordCount(""));
+        assertEquals(0, new WordCountExample().getWordCount(""));
     }
 
     @Test
     void TestWithSingleCharacters() {
-        assertEquals(5, new Foo().getWordCount("a b c d e     $     ?"));
+        assertEquals(5, new WordCountExample().getWordCount("a b c d e     $     ?"));
     }
 
     @Test
     void TestWithSingleCharactersAndSpacesAround() {
-        assertEquals(5, new Foo().getWordCount("     a b c d e     $     ?    "));
+        assertEquals(5, new WordCountExample().getWordCount("     a b c d e     $     ?    "));
     }
 }
