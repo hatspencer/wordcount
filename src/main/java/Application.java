0fd154c52a3e356c48010c6d897195dc5eecda6e
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 10:52:10 +0200

    rename entry point to 'Application'

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
 
-public class WordCount {
+public class Application {
 
     public static void main(String[] args) {
 
