7d0b2a348936d18ddb59b6861d286b7ed6478e41
wernergaisbauer <werner.gaisbauer@gmail.com>
2020-08-31 10:06:20 +0200

    Iteration 1 - WIP

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 https://ccd-school.de/coding-dojo/agility-katas/word-count-i/
+
+We made the following assumptions:
+
+* Input is split at each whitespace.
+* Words are only counted if they stretch from (a-z,A-Z).
--- src/main/java/Foo.java
+++ src/main/java/Foo.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Foo {
+
+    public static void main(String[] args) {
+        System.out.println("Enter text: ");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int wordCount = 0;
+        String[] split = text.split("\\s");
+
+        for (String s : split) {
+            boolean matches = Pattern.matches("[a-zA-Z]*", s);
+            if (matches) {
+                wordCount++;
+            }
+        }
+
+        System.out.println("Number of words: " + wordCount);
+    }
+
 }
