5d2c4d050ecc44b0da5650ca907637968ed3746a
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 13:28:05 +0200

    rename variable to "stopWords"

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -15,12 +15,12 @@ public class Application {
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(System.in));
 
-        List<String> lines = Files.readAllLines(Paths.get("stopwords.txt"), StandardCharsets.UTF_8);
+        List<String> stopWords = Files.readAllLines(Paths.get("stopwords.txt"), StandardCharsets.UTF_8);
 
         try {
             System.out.println("Enter Text: ");
             // System.out.println("Number of words: " + wc.countValidWords(reader.readLine()));
-            System.out.println("Number of words (when processed against stop words): " + wc.countValidWords(reader.readLine(), lines));
+            System.out.println("Number of words (when processed against stop words): " + wc.countValidWords(reader.readLine(), stopWords));
         } catch (IOException e) {
             System.out.println("Could not process request, please try again");
         }
