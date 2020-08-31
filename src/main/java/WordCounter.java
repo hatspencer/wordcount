d417f50c0e2eff2f7926c801fdc1f40f44bcf5a8
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 16:06:13 +0200

    adds countUniqueWords method

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,28 +1,37 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class WordCounter {
 
     public long countValidWords(String input) {
         List<String> wordList = Arrays.asList(input.trim().split("\\s+"));
-        return wordList.stream().filter(word ->  word.matches( "[a-zA-Z]+" )).count();
-    }
-
-    public long countValidWords(String input, List<String> stopWords) {
-        List<String> wordList = Arrays.asList(input.trim().split("\\s+"));
         return wordList.stream()
-                .filter(word ->  !stopWords.contains(word))
                 .filter(word ->  word.matches( "[a-zA-Z]+" ))
+                .map(word -> word.replace(".", ""))
+                .map(word -> word.replace("-", " "))
                 .count();
     }
 
-/*
-    private long findUniqueWords(String input) {
-        Map<String, Integer> uniqueCount;
-        // use map key = word, value = count.
-        return 0L;
+    public long countValidWords(String input, List<String> stopWords) {
+        List<String> wordList = Arrays.asList(sanitizeString(input).split("\\s+"));
+        return wordList.stream()
+                .filter(word -> !stopWords.contains(word))
+                .filter(word -> word.matches("[a-zA-Z]+")).count();
     }
-*/
 
+    public long countUniqueWords(String input, List<String> stopWords) {
+        List<String> wordList = Arrays.asList(sanitizeString(input).split("\\s+"));
+        List<String> santizedList =  wordList.stream()
+                .filter(word -> !stopWords.contains(word))
+                .filter(word -> word.matches("[a-zA-Z]+")).collect(Collectors.toList());
+        Set<String> set = new HashSet<>(santizedList);
+        return set.size();
+    }
+
+    private String sanitizeString(String input) {
+        return input.replace(".", "").replace("-", " ");
+    }
 }
