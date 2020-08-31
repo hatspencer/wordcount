e25849f0d976faedf637c6d6d9c971e95a258692
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 11:16:47 +0200

    add test for word counter: iteration one

--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,7 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class WordCounterTest {
 
+    @Test
+    public void countInputWithOnlyValidWords() {
+        WordCounter wc = new WordCounter();
+        String str = "mary has a litle lamb";
+        long numOfWords = wc.countValidWords(str);
+        assertEquals(5, numOfWords);
+    }
 
-
+    @Test
+    public void countInputWithOnlyInvalidWords() {
+        WordCounter wc = new WordCounter();
+        String str = "mary has a litle !lamb";
+        long numOfWords = wc.countValidWords(str);
+        assertEquals(4, numOfWords);
+    }
 }
 
 
