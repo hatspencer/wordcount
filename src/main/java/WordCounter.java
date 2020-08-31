b6a04d0b266dfc23a24db36534f78990e8fe0d96
JonCasey <jon.casey.dev@gmail.com>
2020-08-31 12:01:37 +0200

    overload count method to support stop words

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -7,4 +7,13 @@ public class WordCounter {
         List<String> wordList = Arrays.asList(input.trim().split("\\s+"));
         return wordList.stream().filter(word ->  word.matches( "[a-zA-Z]+" )).count();
     }
+
+    long countValidWords(String input, List<String> stopWords) {
+        List<String> wordList = Arrays.asList(input.trim().split("\\s+"));
+
+        return wordList.stream()
+                .filter(word ->  stopWords.contains(word))
+                .filter(word ->  word.matches( "[a-zA-Z]+" ))
+                .count();
+    }
 }
