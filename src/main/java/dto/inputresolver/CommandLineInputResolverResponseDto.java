d85f9f0582981dd57eb2209400b944733c265f13
BociocTitus <titus.bocioc@gmail.com>
2020-10-21 16:57:53 +0200

    Dictionary moving to an input argument - (Work in Progress)

--- src/main/java/dto/inputresolver/CommandLineInputResolverResponseDto.java
+++ src/main/java/dto/inputresolver/CommandLineInputResolverResponseDto.java
@@ -3,10 +3,12 @@ package dto.inputresolver;
 public class CommandLineInputResolverResponseDto {
     private final String fileName;
     private final boolean withIndex;
+    private final String dictionaryFileName;
 
-    public CommandLineInputResolverResponseDto(final String fileName, final boolean withIndex) {
+    public CommandLineInputResolverResponseDto(final String fileName, final boolean withIndex, final String dictionaryFileName) {
         this.fileName = fileName;
         this.withIndex = withIndex;
+        this.dictionaryFileName = dictionaryFileName;
     }
 
     public String getFileName() {
@@ -15,5 +17,9 @@ public class CommandLineInputResolverResponseDto {
 
     public boolean isWithIndex() {
         return withIndex;
+    }
+
+    public String getDictionaryFileName() {
+        return dictionaryFileName;
     }
 }
--- src/main/java/utils/inputresolver/CommandLineInputResolver.java
+++ src/main/java/utils/inputresolver/CommandLineInputResolver.java
@@ -2,21 +2,45 @@ package utils.inputresolver;
 
 import dto.inputresolver.CommandLineInputResolverResponseDto;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class CommandLineInputResolver {
+    private final String dictionaryNamingRegex = "";
+    private final List<String> allowedParametersRegularExpressions;
+
+    public CommandLineInputResolver() {
+        allowedParametersRegularExpressions = new ArrayList<>();
+        allowedParametersRegularExpressions.add("-index");
+        allowedParametersRegularExpressions.add("-dictionary=[a-zA-Z]+.txt");
+    }
+
     public CommandLineInputResolverResponseDto resolveCommandLineArguments(final String[] args) {
         final List<String> strings = Arrays.asList(args);
         boolean withIndex = false;
         String fileName = null;
+        String dictionaryArgument = null;
+
+        String argument0;
+        String argument1;
 
         switch (strings.size()) {
             case 0:
                 break;
+            case 1:
+                final String argument = strings.get(0);
+                if (argument.equals("-index")) {
+                    withIndex = true;
+                } else {
+                    fileName = argument;
+                }
+                break;
             case 2:
-                final String argument0 = strings.get(0);
-                final String argument1 = strings.get(1);
+                argument0 = strings.get(0);
+                argument1 = strings.get(1);
                 if (argument0.equals("-index")) {
                     withIndex = true;
                     fileName = argument1;
@@ -28,18 +52,31 @@ public class CommandLineInputResolver {
                     withIndex = true;
                 }
                 break;
-            case 1:
-                final String argument = strings.get(0);
-                if (argument.equals("-index")) {
-                    withIndex = true;
-                } else {
-                    fileName = argument;
-                }
-                break;
+            case 3:
+                findArgumentByName(strings, "-index");
+                withIndex = true;
+                dictionaryArgument = findArgumentByName(strings, dictionaryNamingRegex);
+                fileName = strings.stream().filter(candidate -> !candidate.matches("-index") && !candidate.matches(dictionaryNamingRegex))
+                        .collect(toList())
+                        .get(0);
+
             default:
                 throw new RuntimeException("Program has too many arguments: " + strings);
         }
 
-        return new CommandLineInputResolverResponseDto(fileName, withIndex);
+        return new CommandLineInputResolverResponseDto(fileName, withIndex, dictionaryArgument);
+    }
+
+    private String findArgumentByName(final List<String> strings, final String argumentName) {
+        final List<String> collectedArguments = strings
+                .stream()
+                .filter(candidate -> candidate.matches(argumentName))
+                .collect(toList());
+        if (collectedArguments.size() != 1) {
+            throw new RuntimeException("Invalid argument list");
+        }
+
+        return collectedArguments
+                .get(0);
     }
 }
