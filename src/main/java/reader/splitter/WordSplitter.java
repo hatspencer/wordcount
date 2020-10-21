1d154267b32da8ac69dc2afe9ef4a5855d1e520d
BociocTitus <titus.bocioc@gmail.com>
2020-10-21 13:58:15 +0200

    Modified regex for iteration 5

--- src/main/java/reader/splitter/WordSplitter.java
+++ src/main/java/reader/splitter/WordSplitter.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WordSplitter implements IWordSplitter {
-    private static final String MULTIPLE_SPACES_REGEX = "[ +\\-+]";
+    private static final String MULTIPLE_SPACES_REGEX = " +";
 
     public List<String> splitTextIntoWords(final String text) {
         return Arrays.asList(text.split(MULTIPLE_SPACES_REGEX));
