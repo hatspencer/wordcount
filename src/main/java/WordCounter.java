4f436701cdb2368dc94cbaf8a7cde39f6630c12f
sandor teller <sandor.teller@kuka.com>
2020-10-29 09:55:22 +0100

    implemented splitText and countWords methods added unit tests to these methods updated assumptions renamed classes to be more descriptive

---
+++ src/main/java/WordCounter.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class WordCounter {
+
+
+    public String[] splitText(String text) {
+        return text.split(" ");
+    }
+
+    public long countWords(String[] words) {
+        return Arrays.stream(words).filter(word -> word.matches("[A-Za-z]+")).count();
+    }
+}
---
+++ src/test/java/WordCounterTest.java
@@ -0,0 +1,53 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class WordCounterTest {
+
+    private WordCounter wordCounter;
+
+    @Before
+    public void init() {
+        wordCounter = new WordCounter();
+    }
+
+    @Test
+    public void givenEmptyString_returnOneElementArray() {
+        String text = "";
+        String[] expected = new String[1];
+        expected[0] = "";
+        String[] actual = wordCounter.splitText(text);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void givenOneWord_returnOne() {
+        String[] words = new String[1];
+        words[0] = "word";
+        long expected = 1;
+        long actual = wordCounter.countWords(words);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenInvalidWord_returnZero() {
+        String[] words = new String[]{"wo3rd","234"};
+        long expected = 0;
+        long actual = wordCounter.countWords(words);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void givenDuplicateWords_returnTwo() {
+        String[] words = new String[]{"word","word"};
+        long expected = 2;
+        long actual = wordCounter.countWords(words);
+
+        assertEquals(expected, actual);
+    }
+}
