aaf0fbe559446ae106b5d0b97ea35d22ba56db06
Roman Maly <roman.maly@globallogic.com>
2020-11-05 10:10:27 +0100

    Hide constructor.

--- src/main/java/at/erste/SplitWordCounterUtil.java
+++ src/main/java/at/erste/SplitWordCounterUtil.java
@@ -6,6 +6,11 @@ public final class SplitWordCounterUtil {
 
     public static final Pattern PATTERN_TO_MATCH = Pattern.compile("[a-z]+");
 
+
+    private SplitWordCounterUtil() {
+        //Hide default constructor for UTIL class
+    }
+
     public static Integer countWords(String sentence) {
         int result = 0;
         if (sentence != null) {
